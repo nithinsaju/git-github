4a8f01d97a3dd5f3082a6e60e77b700f4cd75d08
nithinsaju <nithinsaju@gamil.com>
2019-06-12 19:29:52 +0530

    coedfile -13 commit by 2second

--- codefile.py
+++ codefile.py
@@ -1,9 +1,9 @@
 print("my local git-repository")
-if hungry="yes":
+print("2second BRANCH")
+if hungry=="y":
     print("eat samosa")
     print("eat pizza")
     print("eat burger")
 else: 
     print("skip dining")
 
-print("you're in Master Branch")
